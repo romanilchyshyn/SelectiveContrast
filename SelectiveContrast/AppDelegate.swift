81d56b4e635cc18ef15b12b9ba7f4ee5daf6f1ac
Roman Ilchyshyn <romanilchyshyn@gmail.com>
2016-09-24 22:04:20 +0300

    Host app created.

---
+++ SelectiveContrast/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  SelectiveContrast
+//
+//  Created by Roman Ilchyshyn on 9/24/16.
+//  Copyright © 2016 Roman Ilchyshyn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+}
+
